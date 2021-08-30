2d722a1e0b803732bee3ea4d9249a5225d37f4c4
TikvaYosef <tikvayosef13@gmail.com>
2021-08-30 14:06:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+
+
+
+// כתבו פונקציה שקולטת 5 מספרים מהמשתמש, מחשבת סכום וממוצע ומדפיסה למסך את הערכים שלהם. בצע בעזרת לולאה
+// var numbers = [Number(prompt("number")), Number(prompt("number")), Number(prompt("number")), Number(prompt("number")), Number(prompt("number"))]
+
+
+// var sum = 0
+// var avg = sum / numbers.length
+// function valuenum() {
+//     for (var i = 0; i < numbers.length; i++) {
+//         sum = sum + numbers[i]
+//         var avg = sum / numbers.length
+//         console.log(sum)
+//         console.log(avg)
+//         document.write(`the sum:${sum} avg`)
+
+//     }
+// }
+// valuenum(numbers)
+
+
+
+// כתבו פונקציה אשר קולטת מהמשתמש מספר שלם מהמשתמש ומדפיסה את ספרותיו. לדוגמא אם המשתמש הכניס 673. אז הפונקציה תדפיס 6 אחר כך 7 אחרכך 3.
+
+// var userInput = Number(prompt("number"))
+// var num = 0
+// function numberalone() {
+//     while (userInput > 0) {
+//         var modulu = userInput % 10
+//         userInput = (userInput / 10) - (modulu / 10)
+        
+//         console.log(modulu)
+//     }
+    
+    
+// }
+// numberalone(userInput)
+
+
+
+
+// var userInput = Number(prompt("enter a number"))
+// function printnum(userInput){
+//     while (userInput>0) {
+//         var modulu = userInput % 10
+//         console.log();
+//         var userInput = (userInput / 10) - (modulu / 10)
+//         console.log(modulu);
+//     }
+    
+// }
+// printnum(userInput)
+
+
+
